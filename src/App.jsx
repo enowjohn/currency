0c7ae16665d4ec25fcd34653201e9ce30e0b1d12
enow john enowbi <johnwashinton88@gmail.com>
2024-08-02 16:36:49 +0100

    initializing

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import HomePage from './components/pages/HomePage';
+import './App.css';
+import './components/CoverPage'
+
+const App = () => (
+  <div className="App">
+    <HomePage />
+  </div>
+);
+export default App;
